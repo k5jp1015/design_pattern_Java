8faba7fbaeba55771fa7f7f16c5ca527650905be
k5jp1015 <k5.magami@gmail.com>
2017-09-06 21:50:42 +0900

    7_Builder 途中まで

---
+++ 7_Builder/src/pkg/builder/Director.java
@@ -0,0 +1,22 @@
+package pkg.builder;
+
+public class Director {
+    private Builder builder;
+    public Director(Builder builder){
+        this.builder = builder;
+    }
+
+    public void construct(){                //文書構築
+        builder.makeTitle("ごはん");         //タイトル
+        builder.makeString("今日の朝飯");    //文字列
+        builder.makeItems(new String[]{     //箇条書き
+            "ごはん",
+            "味噌汁"
+        });
+        builder.makeString("今日の夕飯");
+        builder.makeItems(new String[]{
+            "ラーメン"
+        });
+        builder.close();
+    }
+}
